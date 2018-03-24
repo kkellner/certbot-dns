68d03d7b1f8e1597dac7a59f6bfe4f030a5355aa
Kurt Kellner <kkellner@ecsteam.com>
2018-03-24 01:44:24 -0600

    Test adding certs to another repo

--- pipeline/generate_cert.sh
+++ pipeline/generate_cert.sh
@@ -4,20 +4,26 @@ set -eux
 
 CERT_OUTPUT_DIR=letsencrypt
 
-certbot certonly \
-     --config-dir "${CERT_OUTPUT_DIR}" \
-     --work-dir "${CERT_OUTPUT_DIR}" \
-     --logs-dir "${CERT_OUTPUT_DIR}" \
-     --non-interactive \
-     --agree-tos \
-     --manual-public-ip-logging-ok \
-     --expand \
-     --dns-route53 \
-     --server https://acme-staging-v02.api.letsencrypt.org/directory \
-     --email "${EMAIL_ADDRESS}" \
-     --domains "${DOMAINS}"
+# certbot certonly \
+#      --config-dir "${CERT_OUTPUT_DIR}" \
+#      --work-dir "${CERT_OUTPUT_DIR}" \
+#      --logs-dir "${CERT_OUTPUT_DIR}" \
+#      --non-interactive \
+#      --agree-tos \
+#      --manual-public-ip-logging-ok \
+#      --expand \
+#      --dns-route53 \
+#      --server https://acme-staging-v02.api.letsencrypt.org/directory \
+#      --email "${EMAIL_ADDRESS}" \
+#      --domains "${DOMAINS}"
 
+mkdir -p ./letsencrypt/live/den.ecsteam.io
 
-ls -alR
+mkdir -p cert-repo/proxy-certs
+
+cp ./letsencrypt/live/den.ecsteam.io/fullchain.pem cert-repo/proxy-certs
+cp ./letsencrypt/live/den.ecsteam.io/privkey.pem cert-repo/proxy-certs
+
+ls -alR cert-repo
 
 
