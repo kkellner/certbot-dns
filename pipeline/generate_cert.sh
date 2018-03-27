3affe37ca19ffd2717ebaf7c3ca154dd69d556dc
Kurt Kellner <kkellner@ecsteam.com>
2018-03-26 20:11:20 -0400

    Make real certs

--- pipeline/generate_cert.sh
+++ pipeline/generate_cert.sh
@@ -4,27 +4,27 @@ set -eux
 
 CERT_OUTPUT_DIR=letsencrypt
 
-# certbot certonly \
-#      --config-dir "${CERT_OUTPUT_DIR}" \
-#      --work-dir "${CERT_OUTPUT_DIR}" \
-#      --logs-dir "${CERT_OUTPUT_DIR}" \
-#      --non-interactive \
-#      --agree-tos \
-#      --manual-public-ip-logging-ok \
-#      --expand \
-#      --dns-route53 \
-#      --server https://acme-staging-v02.api.letsencrypt.org/directory \
-#      --email "${EMAIL_ADDRESS}" \
-#      --domains "${DOMAINS}"
+certbot certonly \
+     --config-dir "${CERT_OUTPUT_DIR}" \
+     --work-dir "${CERT_OUTPUT_DIR}" \
+     --logs-dir "${CERT_OUTPUT_DIR}" \
+     --non-interactive \
+     --agree-tos \
+     --manual-public-ip-logging-ok \
+     --expand \
+     --dns-route53 \
+     --server https://acme-staging-v02.api.letsencrypt.org/directory \
+     --email "${EMAIL_ADDRESS}" \
+     --domains "${DOMAINS}"
 
 # temp to get some changed files
-mkdir -p ./letsencrypt/live/den.ecsteam.io
-date >> ./letsencrypt/live/den.ecsteam.io/fullchain.pem
-date >> ./letsencrypt/live/den.ecsteam.io/privkey.pem
+#mkdir -p ./letsencrypt/live/den.ecsteam.io
+#date >> ./letsencrypt/live/den.ecsteam.io/fullchain.pem
+#date >> ./letsencrypt/live/den.ecsteam.io/privkey.pem
 
-cp ./letsencrypt/live/den.ecsteam.io/fullchain.pem cert-repo/proxy-certs
-cp ./letsencrypt/live/den.ecsteam.io/privkey.pem cert-repo/proxy-certs
+#cp ./letsencrypt/live/den.ecsteam.io/fullchain.pem cert-repo/proxy-certs
+#cp ./letsencrypt/live/den.ecsteam.io/privkey.pem cert-repo/proxy-certs
 
-ls -alR cert-repo
+ls -alR ${CERT_OUTPUT_DIR}
 
 
