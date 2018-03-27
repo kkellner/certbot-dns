e5758b61cb752f62573c9c912e5d44cd7df22325
Kurt Kellner <kkellner@ecsteam.com>
2018-03-26 22:58:43 -0400

    Param the api endpoint to allow for staging / production

--- pipeline/generate_cert.sh
+++ pipeline/generate_cert.sh
@@ -7,7 +7,6 @@ CERT_WORK_DIR=letsencrypt_work
 
 if [ -d cert-repo/${CERT_OUTPUT_DIR} ]; then
     cp -R cert-repo/${CERT_OUTPUT_DIR} .
-    ls -alR ${CERT_OUTPUT_DIR}
 fi 
 
 certbot certonly \
@@ -19,18 +18,7 @@ certbot certonly \
      --manual-public-ip-logging-ok \
      --expand \
      --dns-route53 \
-     --server https://acme-staging-v02.api.letsencrypt.org/directory \
+     --server ${LETSENCRYPT_API_ENDPOINT} \
      --email "${EMAIL_ADDRESS}" \
      --domains "${DOMAINS}"
-
-# temp to get some changed files
-#mkdir -p ./letsencrypt/live/den.ecsteam.io
-#date >> ./letsencrypt/live/den.ecsteam.io/fullchain.pem
-#date >> ./letsencrypt/live/den.ecsteam.io/privkey.pem
-
-#cp ./letsencrypt/live/den.ecsteam.io/fullchain.pem cert-repo/proxy-certs
-#cp ./letsencrypt/live/den.ecsteam.io/privkey.pem cert-repo/proxy-certs
-
-ls -alR ${CERT_OUTPUT_DIR}
-
 
