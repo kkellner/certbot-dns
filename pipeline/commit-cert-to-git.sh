196cecabc8f601c552abb99192f9a9f272d0f8b3
Kurt Kellner <kkellner@ecsteam.com>
2018-03-26 20:30:19 -0400

    add dereference to cp to clone a sym link

--- pipeline/commit-cert-to-git.sh
+++ pipeline/commit-cert-to-git.sh
@@ -6,9 +6,9 @@ CERT_OUTPUT_DIR=letsencrypt
 
 git clone cert-repo cert-repo-modified
 
-# Copy all the generated certs into the repo
+# Copy all the generated certs into the repo ()
 mkdir -p cert-repo-modified/proxy-certs
-cp -R ${CERT_OUTPUT_DIR}/live/* cert-repo-modified/proxy-certs
+cp -R --dereference ${CERT_OUTPUT_DIR}/live/* cert-repo-modified/proxy-certs
 
 cd cert-repo-modified
 
